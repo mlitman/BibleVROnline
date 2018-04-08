f016fcfa5fea4c19e78dce2bc0601dcaab972971
mlitman <awesomefat@gmail.com>
2018-04-08 00:29:14 -0700

    bandrada 04/08/2018 12:29am
    Changed grass material
    Added new script "drive" to output up, down, right, and left for input from the touchpad.    Could not add to an object

---
+++ BibleVR Online/Assets/drive.cs
@@ -0,0 +1,69 @@
+//From Touchpad Splitting - How To Make Games For The HTC Vive In Unity with C# (E03)
+//Should read touch for four directions, need to connect to player movement
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+	public SteamVR_Controller.Device controller
+    {
+		get
+        {
+            return SteamVR_Controller.Input((int)GetComponent<SteamVR_TrackedObject>().index);
+        }
+    }
+	public TouchPosition CurrentTouchPosition ()
+    {
+        Vector2 pos = controller.GetAxis(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad);
+        bool isTop = pos.y >= 0;
+        bool isRight = pos.x >= 0;
+
+		if (isTop)
+        {
+			if (isRight)
+            {
+				if (pos.y > pos.x)
+                {
+                    return TouchPosition.Up;
+                }
+                else if (pos.y < -pos.x)
+                {
+                    return TouchPosition.Left;
+                }
+            }
+        }
+        else
+        {
+            if (isRight)
+            {
+                if (-pos.y > pos.x)
+                {
+                    return TouchPosition.Down;
+                }
+                else if (-pos.y < pos.x)
+                {
+                    return TouchPosition.Right;
+                }
+            }
+            else
+            {
+                if (-pos.y > -pos.x)
+                {
+                    return TouchPosition.Down;
+                }
+                else if (-pos.y < -pos.x)
+                {
+                    return TouchPosition.Left;
+                }
+            }
+        }
+
+        return TouchPosition.Off;
+    }
+}
+
+public enum TouchPosition
+{
+    Off, Up, Down, Left, Right
+}
